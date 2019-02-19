f48dc9c64cbe5d1073e8018625f75525c553bec0
李泉 <quanlidavid@gmail.com>
2019-02-19 08:45:02 +0800

    complete 2 charpter

---
+++ src/main/java/com/example/demo/model/AyUser.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+/**
+ * 用户表
+ */
+public class AyUser {
+    //主键
+    private String id;
+    //用户名
+    private String name;
+    //密码
+    private  String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.demo;
 
+import com.example.demo.model.AyUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * Mysql 集成sping boot简单测试
+     */
+    @Test
+    public void mySqlTest() {
+        String sql = "select id,name,password from ay_user";
+        List<AyUser> userList = jdbcTemplate.query(sql, new RowMapper<AyUser>() {
+            @Override
+            public AyUser mapRow(ResultSet resultSet, int i) throws SQLException {
+                AyUser user = new AyUser();
+                user.setId(resultSet.getString("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        });
+        System.out.println("查询成功：");
+        for (AyUser user : userList) {
+            System.out.println("[ID]:" + user.getId() + "  [name]:" + user.getName());
+
+        }
+    }
 
     @Test
     public void contextLoads() {
